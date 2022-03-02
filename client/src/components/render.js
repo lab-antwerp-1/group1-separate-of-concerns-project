b50ad9fe663f8504da254b7dca25ea0a08c932ae
hansmbua <hansmbua@yahoo.com>
2022-03-02 23:40:41 +0100

    added a scrolBar if items are more then 3

--- client/src/components/render.js
+++ client/src/components/render.js
@@ -3,18 +3,24 @@ export const renderList = (array = []) => {
   const ol = document.createElement('ol');
   for (const object of array) {
     const li = document.createElement('li');
-    li.innerHTML = `<div>
-                        <h4>Book Title:</h4>
-                        <p> ${object.bookTitle}\n</p>
-                    </div>
-                    <div>
-                        <h4>Author:</h4>
-                        <p> ${object.author}\n</p>
-                    </div>
-                    <div>
-                        <h4>Summary:</h4>
-                        <p> ${object.summary}\n</p>
-                    </div>`;
+    li.innerHTML = `
+    <div >
+           <div>
+              <h4>Book Title:</h4>
+              <p> ${object.bookTitle}\n</p>
+          </div>
+          <div>
+              <h4>Author:</h4>
+              <p> ${object.author}\n</p>
+         </div>
+      <div>
+          <h4>Summary:</h4>
+         <p> ${object.summary}\n</p>
+      </div>
+    
+    </div>
+    
+               `;
     ol.appendChild(li);
   }
   return ol;
