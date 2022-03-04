af0398d1adb736ad0ab5a16599e9644fa32471d9
hansmbua <hansmbua@yahoo.com>
2022-03-04 14:37:00 +0100

    fixed scroll and added photos

--- client/src/components/render.js
+++ client/src/components/render.js
@@ -2,8 +2,7 @@
 export const renderList = (array = []) => {
   const ol = document.createElement('ol');
   for (const object of array) {
-    let randomImage = Math.floor((Math.random() * 4) + 0);
-      console.log(randomImage);
+    const randomImage = Math.floor((Math.random() * 4) + 0);
     const li = document.createElement('li');
     li.innerHTML = `
            <div class = row>
